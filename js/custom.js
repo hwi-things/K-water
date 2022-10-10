9474acd0458f8011e8e3823257e650ff570355de
hwi-things <hwi_things@naver.com>
2022-10-11 06:24:22 +0900

    풀페이지 적용

--- js/custom.js
+++ js/custom.js
@@ -10,21 +10,15 @@ if(jQuery.browser.mobile==true){
 
 $(function () {
     $("#fullpage").fullpage({
-        anchors: ["section1", "section2", "section3", "section4", "section5", "section6"],
         navigation: true,
-        navigationPosition: "left",
-        afterLoad: function (anchorLink, index) {
-            //console.log('현재섹션번호는'+index+anchorLink);
-            //변수activeMenu에 li를 저장
-            var activeMenu;
-            if (index == 1) {
-                //변수 activeMenu 에 첫번째 li 저장
-                activeMenu= $("#menu").find("li").first();
-            } else {
-                activeMenu = $("#menu").find("li").eq(index - 1);
-            }
-            activeMenu.addClass("active").siblings().removeClass("active");
-        },
+        navigationPosition: "right",
+    anchors:['slide1',
+    'slide2',
+    'slide3',
+    'slide4',
+    'slide5',
+    'slide6',
+    ],
     });
 
 });
