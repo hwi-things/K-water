9338427d71eeb3c2770b31bb07f34e205912b394
hwi-things <hwi_things@naver.com>
2022-10-11 03:46:04 +0900

    세부 css 수정

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,31 @@ if(jQuery.browser.mobile==true){
     location.href="./mobile/"
 }
 // console.log(location.href);
+
+
+
+// fullpage
+
+$(function () {
+    $("#fullpage").fullpage({
+        anchors: ["section1", "section2", "section3", "section4", "section5", "section6"],
+        navigation: true,
+        navigationPosition: "left",
+        afterLoad: function (anchorLink, index) {
+            //console.log('현재섹션번호는'+index+anchorLink);
+            //변수activeMenu에 li를 저장
+            var activeMenu;
+            if (index == 1) {
+                //변수 activeMenu 에 첫번째 li 저장
+                activeMenu= $("#menu").find("li").first();
+            } else {
+                activeMenu = $("#menu").find("li").eq(index - 1);
+            }
+            activeMenu.addClass("active").siblings().removeClass("active");
+        },
+    });
+
+});
+
+
+
